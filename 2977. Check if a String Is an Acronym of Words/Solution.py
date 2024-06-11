8898216559b27fd77adea27dee9d07f77aca6027
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-06-11 19:23:07 +0530

    Python3 Solution
            Submission Statistics:
            Question #: 2977
            Language: Python3
            Runtime: 68 ms
            Runtime percentile: 6.56
            Memory: 16.492 MB
            Memory percentile: 87.39

---
+++ 2977. Check if a String Is an Acronym of Words/Solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAcronym(self, words: List[str], s: str) -> bool:
+        res = ""
+        for word in words:
+            res += word[0]
+        return res==s
+        
