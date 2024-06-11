261ce33970f1cf9c48dd8ac263ad19817c08e742
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-06-11 19:15:13 +0530

    Python3 Solution
            Submission Statistics:
            Question #: 1786
            Language: Python3
            Runtime: 201 ms
            Runtime percentile: 78.70
            Memory: 18.3 MB
            Memory percentile: 97.41

--- 1786. Count the Number of Consistent Strings/Solution.py
+++ 1786. Count the Number of Consistent Strings/Solution.py
@@ -1,13 +1,10 @@
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        count=0
-        a=set(allowed)
-        
-        for i in words:
-            for j in i:
-                if j not in allowed:
-                    count+=1
-                    break
-        return len(words)-count
+        allowed = set(allowed)
+        count = 0
+        for word in words:
+            if set(word).issubset(allowed):
+                count += 1
+        return count
 
         
