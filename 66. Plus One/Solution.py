134ce05d008e4020f4d0d441b81575a6b50344a6
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-06-12 19:26:36 +0530

    Python3 Solution
            Submission Statistics:
            Question #: 66
            Language: Python3
            Runtime: 35 ms
            Runtime percentile: 68.67
            Memory: 16.476 MB
            Memory percentile: 75.76

--- 66. Plus One/Solution.py
+++ 66. Plus One/Solution.py
@@ -1,14 +1,9 @@
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:    
-        s0=""
-        for i in digits:
-            s0+=str(i)
-        print(s0)
-        n1=int(s0)
-        n2=n1+1
-        s1=str(n2)
-        l=[]
-        for i in s1:
-            l.append(int(i))
-        return l
-        
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in reversed(range(len(digits))):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        return [1] + digits
