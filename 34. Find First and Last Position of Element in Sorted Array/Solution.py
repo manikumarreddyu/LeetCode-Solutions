77cc919046cce23c8be1268027d79e223c2ca07c
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-06-13 04:05:48 +0530

    Python3 Solution
            Submission Statistics:
            Question #: 34
            Language: Python3
            Runtime: 70 ms
            Runtime percentile: 85.19
            Memory: 17.788 MB
            Memory percentile: 74.25

--- 34. Find First and Last Position of Element in Sorted Array/Solution.py
+++ 34. Find First and Last Position of Element in Sorted Array/Solution.py
@@ -1,10 +1,6 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        try:
-            start = nums.index(target)
-            end = start
-            while end + 1 < len(nums) and nums[end + 1] == target:
-                end += 1
-            return [start, end]
-        except ValueError:
+        if target in nums:
+            return [nums.index(target), len(nums)-1-nums[::-1].index(target)]
+        else:
             return [-1, -1]
