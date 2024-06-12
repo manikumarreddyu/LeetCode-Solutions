38505085e4be6ba210588d2c314195081e646415
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-06-12 19:09:35 +0530

    Python3 Solution
            Submission Statistics:
            Question #: 67
            Language: Python3
            Runtime: 34 ms
            Runtime percentile: 78.77
            Memory: 16.484 MB
            Memory percentile: 90.78

--- 67. Add Binary/Solution.py
+++ 67. Add Binary/Solution.py
@@ -1,5 +1,7 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        sum = int(a, 2) + int(b, 2)
+        sum = int(a,2) + int(b,2)
+        print(sum)
         s = bin(sum)
+        print(s)
         return s[2:]
