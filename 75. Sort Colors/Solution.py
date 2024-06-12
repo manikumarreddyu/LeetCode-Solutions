f48ce4bd77aa4255bba93bd06b2d2ebab5958061
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-06-12 23:58:00 +0530

    Python3 Solution
            Submission Statistics:
            Question #: 75
            Language: Python3
            Runtime: 43 ms
            Runtime percentile: 17.92
            Memory: 16.512 MB
            Memory percentile: 33.92

--- 75. Sort Colors/Solution.py
+++ 75. Sort Colors/Solution.py
@@ -3,9 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(arr)
-        for i in range(n):
-            for j in range(0, n-i-1):
-                if arr[j] > arr[j+1]:
-                    arr[j], arr[j+1] = arr[j+1], arr[j]
+        # n = len(arr)
+        # for i in range(n):
+        #     for j in range(0, n-i-1):
+        #         if arr[j] > arr[j+1]:
+        #             arr[j], arr[j+1] = arr[j+1], arr[j]
+        arr.sort()
         
