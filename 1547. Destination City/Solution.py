fc8d37a9839a7c937b707b702b2375be6af0b09c
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-06-14 20:24:43 +0530

    Python3 Solution
            Submission Statistics:
            Question #: 1547
            Language: Python3
            Runtime: 58 ms
            Runtime percentile: 49.61
            Memory: 16.568 MB
            Memory percentile: 75.93

---
+++ 1547. Destination City/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+
+        sec = set(path[1] for path in paths)
+        fir = set(path[0] for path in paths)
+        ans = sec - fir
+        return ans.pop()
+        
+        
