52d52283b0f2bef61e00c12c24f50acefb07648c
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-06-14 19:37:00 +0530

    Python3 Solution
            Submission Statistics:
            Question #: 3331
            Language: Python3
            Runtime: 49 ms
            Runtime percentile: 41.74
            Memory: 16.54 MB
            Memory percentile: 36.25

---
+++ 3331. Minimum Operations to Exceed Threshold Value I/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        nums=sorted(nums)
+        count=0
+        for i in nums:
+            if i>=k:
+                count+=1
+        return len(nums)-count
+        
