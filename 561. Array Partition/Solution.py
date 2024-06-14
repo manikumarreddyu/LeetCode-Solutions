8d4b08e3a24a776c23a9b1f48dfbba866737dac8
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-06-14 21:01:33 +0530

    Python3 Solution
            Submission Statistics:
            Question #: 561
            Language: Python3
            Runtime: 276 ms
            Runtime percentile: 5.15
            Memory: 18.988 MB
            Memory percentile: 93.96

--- 561. Array Partition/Solution.py
+++ 561. Array Partition/Solution.py
@@ -1,8 +1,11 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         nums.sort()
+        print(nums)
         sm = 0
         while nums :
             p1, p2 = nums.pop(), nums.pop()
+            print(p1,p2)
             sm += p2
+            print(sm)
         return sm
